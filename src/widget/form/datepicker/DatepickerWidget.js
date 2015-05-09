1a1453017d29d60f3f664619f3eee410448b07ed
xpz2ty <xpz2ty@163.com>
2015-05-09 13:33:44 +0800

    日期控件未整合版本，直接取自oniui  datepicker

--- src/widget/form/datepicker/DatepickerWidget.js
+++ src/widget/form/datepicker/DatepickerWidget.js
@@ -1,30 +1,50 @@
 /**
- * Created by zengxianping on 15/5/7.
- * emap日期时间控件
- *
+ * Created by zeng x.p on 15/5/7.
+ * 基于avalon的datepicker的组件进行封装
  * @cnName 日期选择器
  * @enName datepicker
  * @introduce
  *    <p>datepicker组件方便快速创建功能齐备的日历组件，通过不同的配置日历可以满足显示多个月份、通过prev、next切换月份、或者通过下拉选择框切换日历的年份、月份，当然也可以手动输入日期，日历组件也会根据输入域中的日期值高亮显示对应日期等等各种需求</p>
  */
 define(['../BaseFormWidget',
-    "./avalon.getModel",
-    "./avalon.datepicker.lang.js",
-    "text!./DatepickerWidget.html",
-    "./avalon.dropdown.js",
-    "./avalon.slider.js",
-    "css!./oniui-common.css",
-    "css!./DatepickerWidget.css",
-    'text!./DatepickerWidget.html',
-    'css!./DatepickerWidget.css'], function (BaseFormWidget, avalon, holidayDate, sourceHTML) {
+        './avalon.datepicker',
+        'text!./DatepickerWidget.html',
+        'css!./DatepickerWidget.css'], function (BaseFormWidget, datepicker, template) {
     var xtype = "datepicker";
-    var datepickerWidget = new Class({
+    var defaults = {
+        $xtype: xtype,
+        startDay: 1, //@config 设置每一周的第一天是哪天，0代表Sunday，1代表Monday，依次类推, 默认从周一开始
+        minute: 0, //@config 设置time的默认minute
+        hour: 0, //@config 设置time的hour
+        width: 90, //@config 设置日历框宽度
+        showTip: false, //@config 是否显示节日提示
+        disabled: false, //@config 是否禁用日历组件
+        changeMonthAndYear: false, //@config 是否可以通过下拉框选择月份或者年份
+        showOtherMonths: false, //@config 是否显示非当前月的日期
+        numberOfMonths: 1, //@config 一次显示的日历月份数, 默认一次显示一个
+        allowBlank : false, //@config 是否允许日历框为空
+        minDate : null, //@config 最小的可选日期，可以配置为Date对象，也可以是yyyy-mm-dd格式的字符串，或者当分隔符是“/”时，可以是yyyy/mm/dd格式的字符串
+        maxDate : null, //@config 最大的可选日期，可以配置为Date对象，也可以是yyyy-mm-dd格式的字符串，或者当分隔符是“/”时，可以是yyyy/mm/dd格式的字符串
+        stepMonths : 1, //@config 当点击next、prev链接时应该跳过几个月份, 默认一个月份
+        toggle: false, //@config 设置日历的显示或者隐藏，false隐藏，true显示
+        separator: "-", //@config 日期格式的分隔符,默认“-”，可以配置为"/"，而且默认日期格式必须是yyyy-mm-dd
+        calendarLabel: "选择日期", //@config 日历组件的说明label
+        dateCellRender: false, // 是否可以自定义日历单元格内容
+        watermark: true, //@config 是否显示水印文字
+        zIndex: -1, //@config设置日历的z-index
+        showDatepickerAlways: false, //@config是否总是显示datepicker
+        timer: false, //@config 是否在组件中可选择时间
+        widgetElement: "", // accordion容器
+        formatErrorTip: "格式错误"
+    };
+    var DatepickerWidget = new Class({
         Extends: BaseFormWidget,
-        options: {
-            $xtype: xtype
+        options: defaults,
+        //
+        getTemplate: function () {
+            return template;
         }
-
     });
-    datepickerWidget.xtype = xtype;
-    return datepickerWidget;
+    DatepickerWidget.xtype = xtype;
+    return DatepickerWidget;
 });
