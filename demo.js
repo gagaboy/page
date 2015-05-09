b42c02205f46f58b590436b05635b327f5c54e24
xpz2ty <xpz2ty@163.com>
2015-05-09 15:53:37 +0800

    日期控件未整合版本，直接取自oniui  datepicker

--- demo.js
+++ demo.js
@@ -1,24 +1,7 @@
 //require.config({
 //    baseUrl: "src"
 //})
-
-//function deleteRow(row){
-//    var index = parseInt(jQuery(row).parent().attr('rowIndex'));
-//    var r = Page.manager.components.grid.getAttr('data')[index].$model;
-//    window.console.log(r);
-//}
-//avalon.config({loader: false});
-/*
-require.config({
-    paths: {
-        text : "bower_components/text/text",
-        css:"bower_components/require-css/css"
-    }
-    //baseUrl:"src"
-});
-*/
-
-require(["src/Bootstrap"], function () {
+require(["./src/Bootstrap"], function () {
     var inp = Page.create("input", {
         $parentId: 'inputContainer',
         value: '',
@@ -27,14 +10,8 @@ require(["src/Bootstrap"], function () {
         message: '请输入用户名',
         label: '用户名',
         hasError: false,
-        $id: 'username',//,
+        $id: 'username'//,
         //value: 'init'
-        onValueChange:function(){
-            window.console.log('a');
-        }
-    });
-    inp.addEvent('labelClick',function(c){
-        alert(c.getAttr("value"));
     });
     inp.render();
     console.log(inp);
@@ -45,32 +22,14 @@ require(["src/Bootstrap"], function () {
     })
     combo.render();
 
-    var grid = Page.create("simpleGrid", {
-        $id: 'grid',
-        $parentId: 'simpleGrid',
-        header: [{
-            title: '职工号',
-            ctitle: 'zgh'
-
-        }, {
-            title: '起始年月',
-            ctitle: 'qsny'
-        }, {
-            title: '性别',
-            ctitle: 'xb',
-            type: 'ctype'
-
-        }],
-        opColumn: {
-            template: '<button ms-click="opColumn.deleteRow(a)">delete111</button>',
-            deleteRow: function (a) {
-                //alert(a);
-                window.console.log(a);
-            }
-        },
-        data: [{zgh: '123', qsny: '1923-11', xb: "1", xbDisplay: '男'}, {zgh: '345', qsny: '1923-12'}]
-
-
+    var datepicker = Page.create("datepicker", {
+        $parentId: 'datepickerContainer',
+        $id: 'datepicker123456',
+        time: '2015-04-28',
+        onSelect: function(date, vmodel, data) {
+            avalon.log("选中日期后的用户回调")
+            avalon.log(arguments);
+        }
     });
-    grid.render();
+    datepicker.render();
 });
--- src/widget/form/datepicker/DatepickerWidget.js
+++ src/widget/form/datepicker/DatepickerWidget.js
@@ -13,29 +13,32 @@ define(['../BaseFormWidget',
     var xtype = "datepicker";
     var defaults = {
         $xtype: xtype,
-        startDay: 1, //@config 设置每一周的第一天是哪天，0代表Sunday，1代表Monday，依次类推, 默认从周一开始
-        minute: 0, //@config 设置time的默认minute
-        hour: 0, //@config 设置time的hour
-        width: 90, //@config 设置日历框宽度
-        showTip: false, //@config 是否显示节日提示
-        disabled: false, //@config 是否禁用日历组件
-        changeMonthAndYear: false, //@config 是否可以通过下拉框选择月份或者年份
-        showOtherMonths: false, //@config 是否显示非当前月的日期
-        numberOfMonths: 1, //@config 一次显示的日历月份数, 默认一次显示一个
-        allowBlank : false, //@config 是否允许日历框为空
-        minDate : null, //@config 最小的可选日期，可以配置为Date对象，也可以是yyyy-mm-dd格式的字符串，或者当分隔符是“/”时，可以是yyyy/mm/dd格式的字符串
-        maxDate : null, //@config 最大的可选日期，可以配置为Date对象，也可以是yyyy-mm-dd格式的字符串，或者当分隔符是“/”时，可以是yyyy/mm/dd格式的字符串
-        stepMonths : 1, //@config 当点击next、prev链接时应该跳过几个月份, 默认一个月份
-        toggle: false, //@config 设置日历的显示或者隐藏，false隐藏，true显示
-        separator: "-", //@config 日期格式的分隔符,默认“-”，可以配置为"/"，而且默认日期格式必须是yyyy-mm-dd
-        calendarLabel: "选择日期", //@config 日历组件的说明label
-        dateCellRender: false, // 是否可以自定义日历单元格内容
-        watermark: true, //@config 是否显示水印文字
-        zIndex: -1, //@config设置日历的z-index
-        showDatepickerAlways: false, //@config是否总是显示datepicker
-        timer: false, //@config 是否在组件中可选择时间
-        widgetElement: "", // accordion容器
-        formatErrorTip: "格式错误"
+        $opt:{
+            startDay: 1, //@config 设置每一周的第一天是哪天，0代表Sunday，1代表Monday，依次类推, 默认从周一开始
+            minute: 0, //@config 设置time的默认minute
+            hour: 0, //@config 设置time的hour
+            width: 90, //@config 设置日历框宽度
+            showTip: true, //@config 是否显示节日提示
+            disabled: false, //@config 是否禁用日历组件
+            changeMonthAndYear: false, //@config 是否可以通过下拉框选择月份或者年份
+            mobileMonthAndYear: false, //@config PC端可以通过设置changeMonthAndYear为true使用dropdown的形式选择年份或者月份，但是移动端只能通过设置mobileMonthAndYear为true来选择月份、年份
+            showOtherMonths: false, //@config 是否显示非当前月的日期
+            numberOfMonths: 1, //@config 一次显示的日历月份数, 默认一次显示一个
+            allowBlank : false, //@config 是否允许日历框为空
+            minDate : null, //@config 最小的可选日期，可以配置为Date对象，也可以是yyyy-mm-dd格式的字符串，或者当分隔符是“/”时，可以是yyyy/mm/dd格式的字符串
+            maxDate : null, //@config 最大的可选日期，可以配置为Date对象，也可以是yyyy-mm-dd格式的字符串，或者当分隔符是“/”时，可以是yyyy/mm/dd格式的字符串
+            stepMonths : 1, //@config 当点击next、prev链接时应该跳过几个月份, 默认一个月份
+            toggle: false, //@config 设置日历的显示或者隐藏，false隐藏，true显示
+            separator: "-", //@config 日期格式的分隔符,默认“-”，可以配置为"/"，而且默认日期格式必须是yyyy-mm-dd
+            calendarLabel: "选择日期", //@config 日历组件的说明label
+            widgetElement: "", // accordion容器
+            formatErrorTip: "格式错误",
+            onSelect:function(date,vmodel,data){
+                var cmp = Page.manager.components[vmodel.$id];
+                //cmp.fireEvent('onSelect', arguments);
+                cmp.fireEvent('onSelect');
+            }
+        }
     };
     var DatepickerWidget = new Class({
         Extends: BaseFormWidget,
