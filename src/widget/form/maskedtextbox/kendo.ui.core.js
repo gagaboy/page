1b3590831a4c29ed12c9f3d22952fa0a99713b91
baoqin87 <baoqin87@sohu.com>
2015-05-14 11:10:08 +0800

    删除多余的文件

--- src/widget/form/maskedtextbox/kendo.ui.core.js
+++
@@ -1,7 +0,0 @@
-(function(f, define){
-    define([
-        "./kendo.core"
-    ], f);
-})(function(){
-    "bundle all";
-}, typeof define == 'function' && define.amd ? define : function(_, f){ f(); });
