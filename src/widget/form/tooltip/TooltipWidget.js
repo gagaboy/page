27c6a21e49a824f4bc62a6d02630bb52a00771c4
tianlaixhh <tianlaijimo@sina.com>
2015-05-25 21:11:32 +0800

    grid

--- src/widget/form/tooltip/TooltipWidget.js
+++ src/widget/form/tooltip/TooltipWidget.js
@@ -5,13 +5,14 @@ define(['../BaseFormWidget','../../../../lib/kendoui/js/kendo.tooltip', 'css!./T
         options: {
             $xtype: xtype,
             target:"", //绑定tip的对象id
+            content:"",
             showAfter: 100,
             position: "bottom",
             showOn: "mouseenter",
             autoHide: true,
             show:null
         },
-        tipObj:null,
+        tipObj:{},
         _getInputElement: function () {
             if(this.options.target != ""){
                 var input = jQuery("#"+this.options.target);
@@ -22,10 +23,43 @@ define(['../BaseFormWidget','../../../../lib/kendoui/js/kendo.tooltip', 'css!./T
         },
         render: function (opts) {
             var p = jQuery.extend({}, this.options, opts || {})
+            var that = this;
+            if(!p.filter){
+                p.filter = function(){
+                    if(!that.getAttr("content")){
+                        that.tipObj.hide();
+                        return false;
+                    }
+                }
+            }
             if(this._getInputElement()){
-                this.tipObj = this._getInputElement().kendoTooltip(p);
+                this._getInputElement().kendoTooltip(p);
+                this.tipObj = this._getInputElement().data("kendoTooltip");
+                if(!this.getAttr("content")){
+                    this.tipObj.hide();
+                }
+            }
+        },
+        destroy:function(){
+            this.tipObj.destroy()
+            this.parent();
+        },
+        show:function(){
+            this.tipObj.show();
+        },
+        hide:function(){
+            this.tipObj.hide();
+        },
+        _contentChange:function(content,oldContent){
+            this.tipObj.options.content = content;
+            if(!content){
+                this.hide();
+            }else{
+                this.show();
+                this.tipObj.refresh();
             }
         }
+
     });
     TooltipWidget.xtype = xtype;
     return TooltipWidget;
