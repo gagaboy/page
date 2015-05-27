110a0c59c0113310fe44e4b906939a53583ac066
bzksx2 <247792825@qq.com>
2015-05-27 20:09:28 +0800

    bikui

--- src/widget/form/my97date/My97DateWidget.js
+++ src/widget/form/my97date/My97DateWidget.js
@@ -1,9 +1,10 @@
 define(['../BaseFormWidget', 'text!./My97DateWidget.html', 'my97DatePicker'], function (BaseFormWidget, template, my97DatePicker) {
-    var xtype = "my97datepicker";
+    var xtype = "datepicker";
     var My97DateWidget = new Class({
         Extends: BaseFormWidget,
         options: {
             $xtype: xtype,
+            value: null,
             $opts:{
                 doubleCalendar: false,
                 dateFmt:'yyyy-MM-dd',
@@ -22,7 +23,7 @@ define(['../BaseFormWidget', 'text!./My97DateWidget.html', 'my97DatePicker'], fu
             var $opts = {};
             var formOpt = Page.create("baseFormWidget", {}).options;
             for(var key in opts) {
-                if(!(key in formOpt) && key!="$id") {
+                if(!(key in formOpt) && key!="$id" && !key.startsWith("on")) {
                     $opts[key] = opts[key];
                     if("formatDate" == key) {
                         $opts["dateFmt"] = opts[key];
@@ -40,6 +41,10 @@ define(['../BaseFormWidget', 'text!./My97DateWidget.html', 'my97DatePicker'], fu
             this._getInputElement().bind("focus",function(){
                 WdatePicker(that.options.$opts);
             });
+        },
+        _valueChange: function (value) {
+            this.setAttr("display", value);
+            this.validate();//即时校验
         },
         getTemplate: function () {
             return template;
