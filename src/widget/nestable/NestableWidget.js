40eb384b776611aeca7f17c86fd64556bbbd402a
tianlaixhh <tianlaijimo@sina.com>
2015-06-06 15:39:48 +0800

    grid

--- src/widget/nestable/NestableWidget.js
+++ src/widget/nestable/NestableWidget.js
@@ -6,28 +6,14 @@ define(['../Base', 'text!./NestableWidget.html', 'css!./NestableWidget.css'], fu
     var DataTableWidget = new Class({
         Extends: Base,
         options: {
-            $xtype: xtype,
-            autoTable:true
+            $xtype: xtype
             //其他nestable属性
         },
         nestableObj:null,
         render: function () {
             this.fireEvent("beforeRender", [this.vmodel]);
-            var $this = this;
-            if(this.getAttr("autoTable")){//给出容器，如div
-                var tmp = $(this.getTemplate());
-                var e = jQuery("<div></div>").addClass("page_"+$this.getAttr('$xtype'));
-                e.append(tmp);
-                $this.getParentElement().html(e);
-
-                var tableObj = tmp.find("table.table");
-
-                this.nestableObj = jQuery(tableObj).nestable(this.options);//调用nestable
-                $this.element = e[0];
-            }else{//使用已有table渲染
-                this.nestableObj = $this.getParentElement().nestable(this.options);//调用nestable
-            }
-            $this.fireEvent("afterRender", [this.vmodel]);
+            this.nestableObj = this.getParentElement().nestable(this.options);//调用nestable
+            this.fireEvent("afterRender", [this.vmodel]);
             if (this["_afterRender"]) {
                 this["_afterRender"](this.vmodel);
             }
@@ -35,6 +21,9 @@ define(['../Base', 'text!./NestableWidget.html', 'css!./NestableWidget.css'], fu
         },
         getTemplate: function(){
             return template;
+        },
+        getData:function(){
+            return this.getParentElement().nestable("serialize");
         }
     });
     DataTableWidget.xtype = xtype;
