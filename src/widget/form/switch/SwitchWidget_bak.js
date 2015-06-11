78f48e3da1b1406e199928b8a6dda093f190ca2b
tianlaijimo@sina.com <tianlaijimo@sina.com>
2015-06-11 09:56:00 +0800

    校验提示信息

--- src/widget/form/switch/SwitchWidget_bak.js
+++ src/widget/form/switch/SwitchWidget_bak.js
@@ -7,9 +7,7 @@ define(['../BaseFormWidget','text!./SwitchWidget.html','css!./SwitchWidget.css']
             show:true,
             color: '#1AB394',
             checked:false,
-            disabled: false,
-            checkValue:1,// 默认为1,
-            unCheckValue:0// 默认为0
+            disabled: false
         },
         switchObj:{},
         render: function (opts) {
@@ -36,29 +34,18 @@ define(['../BaseFormWidget','text!./SwitchWidget.html','css!./SwitchWidget.css']
             this.switchObj.hide();
         },
         getValue:function(){
-            return this.switchObj.isChecked()?this.options.checkValue:this.options.unCheckValue;
-        },
-        setValue:function(value){
-            if(value!=undefined){
-                var checked = false;
-                if(value==this.options.checkValue){
-                    checked = true;
-                }
-                if(checked!=this.switchObj.isChecked()){
-                    this.switchObj.setPosition(true);
-                    this.setAttr("checked",checked,true);
-                }
-            }
-        },
-        isChecked:function(){
             return this.switchObj.isChecked();
         },
-        check:function(){
-            if(!this.switchObj.isChecked()){
+        setValue:function(checked){
+            if(!checked){
+                checked = false;
+            }
+            if(checked!=this.getAttr("checked")){
                 this.switchObj.setPosition(true);
-                this.setAttr("checked",true,true);
+                this.setAttr("checked",checked,true);
             }
         }
+
     });
     TooltipWidget.xtype = xtype;
     return TooltipWidget;
