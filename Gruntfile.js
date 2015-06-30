60159584835d1427619cc854979117b7a31b909d
baoqin87 <baoqin87@sohu.com>
2015-06-30 17:35:28 +0800

    修改kindeditor路径

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,16 @@ module.exports = function(grunt) {
                     name:"demo",
                     baseUrl: "./",
                     paths: {
-                        text: 'bower_components/text/text',
-                        css: 'bower_components/require-css/css',
+                        "text": 'bower_components/text/text',
+                        "css": 'bower_components/require-css/css',
                         "css-builder": 'bower_components/require-css/css-builder',
                         "normalize": "bower_components/require-css/normalize",
-                        "avalon": "bower_components/oniui/avalon",
+                        "avalon": "bower_components/avalon/avalon",
                         "art": 'lib/artdialog/artDialog.source',
                         "artIframe": 'lib/artdialog/iframeTools.source',
                         "my97DatePicker": "lib/My97DatePicker/WdatePicker",
                         "zTree": "lib/zTree_v3/js/jquery.ztree.all-3.5.min",
-                        "kindeditor": "/lib/kindeditor-4.1.10/kindeditor"
+                        "kindeditor": "lib/kindeditor-4.1.10/kindeditor"
                     },
                     optimize: 'none',
                     preserveLicenseComments:false,
@@ -46,7 +46,7 @@ module.exports = function(grunt) {
 
     require('load-grunt-tasks')(grunt);
     // 注册任务
-    grunt.registerTask('default', ['clean','requirejs','uglify:builda']);
+    grunt.registerTask('default', ['clean','requirejs:build','uglify:builda']);
     //不支持Source Map
     grunt.registerTask('buildNoMap', ['clean','requirejs','uglify:buildb']);
 };
